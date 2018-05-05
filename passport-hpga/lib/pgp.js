c2c8a063c7821253ab63ee0c2ebf9cd9e108f1d1
patgrasso <pgrasso@stevens.edu>
2018-05-04 20:22:37 -0400

    Implement PassportJS strategy for HPGA

---
+++ passport-hpga/lib/pgp.js
@@ -0,0 +1,115 @@
+const openpgp   = require('openpgp');
+
+const KEYSERVER = 'http://gpg.nebrwesleyan.edu';
+const USERID_RE = /^(.*?) ?<(.*?)>$/;
+const PGP_RE = /(\-{5}(?:BEGIN|END) PGP [A-Z]*\-{5}|[a-zA-Z0-9\+\/\=]{1,64})/g;
+
+const hkp       = new openpgp.HKP(KEYSERVER);
+
+/**
+ * Splits a string like "My Name <myname@a.com>" into `name` an `email`, then
+ * returns them in an object.
+ *
+ * TODO: allow the regex to support incomplete strings (e.g. just an email
+ * address, just a name, an email address without arrow brackets, etc.)
+ *
+ * @param {string} userId string like "My Name <myname@a.com>"
+ * @return {object} { name, email } from the userId string
+ */
+function parseUserId(userId) {
+  let match = userId.match(USERID_RE);
+
+  if (match == null) {
+    return null;
+  }
+
+  return {
+    name: match[1],
+    email: match[2]
+  };
+}
+
+/**
+ * Extract the first signature's key ID from the given message.
+ *
+ * @param {openpgp.message.Message | string} msg
+ * @return {string} long PGP key ID in uppercase hex
+ */
+function getSigningKeyId(msg) {
+  if (!(msg instanceof openpgp.message.Message)) {
+    msg = readArmored(msg);
+  }
+
+  let signatures = msg.getSigningKeyIds();
+
+  if (signatures.length <= 0) {
+    throw new Error('No signatures applied to message');
+  }
+
+  return signatures[0].toHex().toUpperCase();
+}
+
+/**
+ * Verifies that the given message was signed by the given public key and the
+ * content of the message matches the given expected text.
+ *
+ * @param {openpgp.message.Message | string} msg
+ * @param {openpgp.packet.PublicKey} publicKey key of mandatory signature
+ * @return {Promise} that returns a { user, email } object, or throws if any
+ *    steps during verification fail
+ */
+async function verify(msg, publicKey) {
+  msg = readArmored(msg)
+
+  let publicKeyId = publicKey.primaryKey.keyid.toHex().toUpperCase();
+
+  let { data, signatures } = await openpgp.verify({
+    message: msg,
+    publicKeys: [ publicKey ]
+  });
+
+  let signatureOfInterest = signatures.find(({ valid, keyid }) => {
+    return valid === true && keyid.toHex().toUpperCase() === publicKeyId;
+  });
+
+  if (signatureOfInterest == null) {
+    throw { 'error': 'Invalid signature' };
+  }
+
+  let user = parseUserId(publicKey.getUserIds()[0]);
+  user.publicKey = publicKey.toPacketlist().write();
+  user.keyId = publicKeyId;
+
+  return { user, data };
+}
+
+/**
+ * Forwards to openpgp.message.readArmored() after doing some string
+ * manipulation to account for missing newlines, etc.
+ *
+ * @param {string} message PGP armored message string
+ * @return {openpgp.message.Message}
+ */
+function readArmored(message) {
+  if (message instanceof openpgp.message.Message) {
+    return message;
+  }
+
+  let lines = message.match(PGP_RE);
+  lines[0] += '\n';
+  return openpgp.message.readArmored(lines.join('\n'));
+}
+
+/**
+ * Calls hkp.lookup() on the keyId, then returns the parsed public key object
+ * from the armored text returned by the key server.
+ *
+ * @param {string} keyId ideally, the long ID of the public key in question
+ * @return {openpgp.key.Key}
+ */
+async function findKey(keyId) {
+  let publicKeyArmored = await hkp.lookup({ keyId });
+  return openpgp.key.readArmored(publicKeyArmored).keys[0];
+}
+
+module.exports = { verify, readArmored, getSigningKeyId, findKey }
---
+++ passport-hpga/lib/strategy.js
@@ -0,0 +1,199 @@
+const Strategy  = require('passport-strategy');
+const crypto    = require('crypto');
+const pgp       = require('./pgp');
+
+const CHALLENGE_LEN = 64;
+const AUTH_HDR_RE   = /^([^ ]*?) (.*)$/
+
+class HPGAStrategy extends Strategy {
+
+  /**
+   * Creates new HPGAStrategy
+   *
+   * @param {function} [options.findKey] called when trying to find a public
+   *    key by its ID. Applications that cache public keys can use this to
+   *    save a trip on each login to a key server
+   * @param {function} [options.onChallenge] called when a challenge is issued
+   * @param {function} [getUser] called just before calling success() to
+   *    transform a "user" (data like name and email @ stored in a PGP public
+   *    key) into a user that the application knows (e.g. findKey user in DB)
+   */
+  constructor(options, getUser) {
+    super();
+    this.name = 'hpga';
+    this._challenges = {};
+    this._findKey = options.findKey;
+    this._onChallenge = options.onChallenge;
+    this._getUser = getUser;
+    this._oneTimeTokens = {};
+  }
+
+  /**
+   * Generate a new challenge string then store it in memory. If the user has
+   * specified option: onChallenge(), call it.
+   *
+   * @return {string} base64-encoded sequence of CHALLENGE_LEN bytes
+   */
+  generateChallenge() {
+    let newChallenge;
+
+    do {
+      newChallenge = crypto.randomBytes(CHALLENGE_LEN).toString('base64');
+    } while (this._challenges[newChallenge] === true);
+
+    this._challenges[newChallenge] = true;
+
+    if (typeof this._onChallenge === 'function') {
+      this._onChallenge(newChallenge);
+    }
+    return newChallenge;
+  }
+
+  /**
+   * Redirect the user to a one-time URL that contains the token. This is to
+   * support the CLI client for HPGA (currently the only safe way of
+   * authenticating using this method). Users whose agent implements HPGA
+   * shouldn't be affected by this, as their agent should redirect them through
+   * this URL automatically, so they shouldn't notice a difference.
+   *
+   * @param {express.Request} req
+   * @param {object} user this will be associated with a random token in memory
+   *    until the token's URL is accessed, at which point it will be removed
+   *    from the cache
+   */
+  redirectToOneTimeURL(req, user) {
+    let token = crypto.randomBytes(24).toString('hex');
+    this._oneTimeTokens[token] = user;
+    return this.redirect(`${req.path}?token=${token}`);
+  }
+
+  /**
+   * Handles the case where a POST request leads to the authenticate()
+   * middleware. This means the 'Authorization' header should be present and
+   * should contain a signed message of a valid challenge that has been
+   * recently issued. This checks that all of these assertions are true.
+   *
+   * @param {express.Request} req
+   */
+  async handleVerification(req, options) {
+    // Split the 'Authorization' header per RFC 7235 into <type> and ...the
+    // rest of the string.
+    let [ _, type, body ] = req.headers['authorization'].match(AUTH_HDR_RE);
+
+    if (type.toUpperCase() !== 'PGP') {
+      return this.fail(null, 400);
+    }
+
+    let msg = pgp.readArmored(body);
+    let publicKey = null;
+
+    try {
+      var signingKeyId = pgp.getSigningKeyId(msg);
+    } catch (e) {
+      return this.error(e.message);
+    }
+
+    // Sometimes apps cache keys (yay!). If so, they may provide a findKey()
+    // function for retrieving a full key by its ID.
+    if (typeof this._findKey === 'function') {
+      try {
+        publicKey = await this._findKey(signingKeyId)
+      } catch (_) {}
+    }
+    // Otherwise, try the key server. Note that this may take a while.
+    if (publicKey == null) {
+      if ((publicKey = await pgp.findKey(signingKeyId)) == null) {
+        return this.error(`Could not find public key for ${signingKeyId}`);
+      }
+    }
+
+    return pgp.verify(msg, publicKey)
+      .then(({ user, data }) => {
+        if (this._challenges[data] !== true) {
+          throw { 'error': 'Message does not contain valid challenge' };
+        }
+        delete this._challenges[data];
+        return user;
+      })
+      .then((user) => {
+        /*
+         * If the app specifies a getUser function (in constructor), use it
+         * to retrieve the correct user (from the app's perspective) before
+         * calling success(). Otherwise, just use the data we obtain from the
+         * public key.
+         */
+        if (typeof this._getUser === 'function') {
+          this._getUser(user, (err, userFromApp) => {
+            if (err) {
+              return this.error(err);
+            }
+            return this.redirectToOneTimeURL(req, userFromApp);
+          });
+        } else {
+          return this.redirectToOneTimeURL(req, user);
+        }
+      })
+      .catch((err) => this.error(err.error !== undefined ? err.error : err));
+  }
+
+  /**
+   * Handles the case where a GET request leads to the authenticate(), where
+   * req.params.token or req.query.token are also defined. If these aren't
+   * defined, you shouldn't be calling this method. Looks up the token in this
+   * instance's token store and calls success() with the user associated with
+   * that token.
+   *
+   * @param {express.Request} req
+   * @param {string} req.params.token
+   * @param {string} req.query.token
+   */
+  handleTokenClaim(req, options) {
+    let token = req.params.token || req.query.token;
+
+    if (token == null) {
+      return this.fail(null, 400);
+    }
+
+    let user = this._oneTimeTokens[token];
+
+    if (user == null) {
+      return this.fail(null, 404);
+    }
+
+    delete this._oneTimeTokens[token];
+    return this.success(user);
+  }
+
+  /**
+   * Using req.method to distinguish which phase of authentication we are in,
+   * GET requests will lead to a new challenge being issued, while POST
+   * requests will check for the 'Authorization' header and verify its contents
+   * to make sure that the signed content refers to a valid challenge.
+   *
+   * User-specified options are referenced for looking up keys by ID and
+   * transforming a key's user data into a user object maintained by the
+   * application.
+   *
+   * @override
+   * @param {express.Request} req
+   */
+  async authenticate(req, options) {
+    if (req.method === 'POST' && req.headers['authorization']) {
+      return this.handleVerification(req, options);
+
+    } else if (req.method === 'POST') {
+      return this.fail(null, 400);
+
+    } else if (req.method === 'GET' && (req.params.token || req.query.token)) {
+      return this.handleTokenClaim(req, options);
+
+    } else if (req.method === 'GET') {
+      return this.fail(`PGP challenge="${this.generateChallenge()}"`);
+    }
+
+    return this.error(`Unsupported method: ${req.method}`);
+  }
+
+}
+
+module.exports = HPGAStrategy;
